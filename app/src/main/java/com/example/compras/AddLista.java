8e4c6984b5ea192eef419afbcf23a625306e5944
Luiz Martins <luiiz.martiins@outlook.com>
2019-04-10 17:10:21 -0300

    Criado o layout para adicionar nova lista

---
+++ app/src/main/java/com/example/compras/AddLista.java
@@ -0,0 +1,22 @@
+package com.example.compras;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class AddLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_lista);
+
+        /*btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }*/
+        };
+    }
+
+
