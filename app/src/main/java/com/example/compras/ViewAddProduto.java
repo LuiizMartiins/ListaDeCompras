a0eec338b0c60913d5186d53fe658339c0022be8
johnvcosta <jonathancosta123@gmail.com>
2019-04-21 02:15:30 -0300

    Alterada nome das classes, activity e content para padronizar, criado AdapterListaProduto, e função de abrir a View de produtos ao clicar na lista. criação de produtos dentro da lista em andamento...

--- app/src/main/java/com/example/compras/ViewAddProduto.java
+++ app/src/main/java/com/example/compras/ViewAddProduto.java
@@ -1,11 +1,7 @@
 package com.example.compras;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,7 +15,7 @@ public class CadastroProduto extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_produto);
+        setContentView(R.layout.activity_view_add_produto);
 
         edtNomeProduto = (EditText)findViewById(R.id.edtNomeProduto);
         edtQntdProduto = (EditText)findViewById(R.id.edtQntdProduto);
