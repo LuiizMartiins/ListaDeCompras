bfe311faa13967acff5b53e8221f882e47949e00
Luiz Martins <luiiz.martiins@outlook.com>
2019-04-30 22:02:26 -0300

    Adicionado titulo nos layouts que não tinham

--- app/src/main/java/com/example/compras/ViewAddProduto.java
+++ app/src/main/java/com/example/compras/ViewAddProduto.java
@@ -2,6 +2,7 @@ package com.example.compras;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -22,5 +23,21 @@ public class ViewAddProduto extends AppCompatActivity {
         edtValorProduto = (EditText)findViewById(R.id.edtValorProduto);
         btnCadastrarProduto = (Button)findViewById(R.id.btnCadastrarProduto);
 
+        btnCadastrarProduto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                salvar();
+            }
+        });
+
+    }
+
+    public void salvar(){
+        Produto p = new Produto();
+        p.setNomeProduto( edtNomeProduto.getText().toString() );
+        p.setQtdProduto(Integer.parseInt(edtQntdProduto.getText().toString()));
+        p.setValor(Double.parseDouble(edtValorProduto.getText().toString()));
+        ProdutoDAO.inserir( p, getIntent().getExtras().getInt("idLista2"), this );
+        this.finish();
     }
 }
